7ee54584df1851a6f3617559158844d09a2b352c
superuser <mymail@maila.com>
2026-01-22 13:02:22 +0100

    update transform-func edge function

---
+++ netlify/edge-functions/transform-func.js
@@ -0,0 +1,25 @@
+export default async (request, context) => {
+  console.log('<transform-func> CALLED')
+  const url = new URL(request.url)
+
+  // const query = url.searchParams.get('method')
+  // console.dir({ url, query })
+  // if (query !== 'transform') {
+  //   return
+  // }
+
+  const response = await context.next()
+  const page = await response.text()
+  const city = context.geo.city
+  const country = context.geo.country.name
+  const regex = /LOCATION_UNKNOWN/i
+
+  context.log(city, country)
+
+  const location = `${city},  ${country}`
+
+  console.dir({ location })
+
+  const updatedPage = page.replace(regex, location)
+  return new Response(updatedPage, response)
+}
